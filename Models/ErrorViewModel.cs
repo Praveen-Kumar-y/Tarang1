49f4fa652b71a88039dd2ba77ea110ee078fe85a
Bharat_gillala_Reddy <Bharat_gillala_Reddy@W105F3QVP3>
2022-11-16 17:10:25 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
